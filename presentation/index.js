410d8b4b4c959adf334e2a328da88373ec9e7a96
Michael Hadley <michael.hadley@liferay.com>
2017-06-01 16:54:38 -0700

    Change quotes

--- presentation/index.js
+++ presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from "react";
+import React from 'react';
 
 // Import Spectacle Core tags
 import {
@@ -12,61 +12,61 @@ import {
   Quote,
   Slide,
   Text
-} from "spectacle";
+} from 'spectacle';
 
 // Import image preloader util
-import preloader from "spectacle/lib/utils/preloader";
+import preloader from 'spectacle/lib/utils/preloader';
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default';
 
 // Require CSS
-require("normalize.css");
-require("spectacle/lib/themes/default/index.css");
+require('normalize.css');
+require('spectacle/lib/themes/default/index.css');
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  city: require('../assets/city.jpg'),
+  kat: require('../assets/kat.png'),
+  logo: require('../assets/formidable-logo.svg'),
+  markdown: require('../assets/markdown.png')
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quartenary: "#CECECE"
+  primary: 'white',
+  secondary: '#1F2022',
+  tertiary: '#03A9FC',
+  quartenary: '#CECECE'
 }, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
+  primary: 'Montserrat',
+  secondary: 'Helvetica'
 });
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+      <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
+        <Slide transition={['zoom']} bgColor='primary'>
+          <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
             Spectacle Boilerplate
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+          <Text margin='10px 0 0' textColor='tertiary' size={1} fit bold>
             open the presentation/index.js file to get started
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide transition={['fade']} bgColor='tertiary'>
+          <Heading size={6} textColor='primary' caps>Typography</Heading>
+          <Heading size={1} textColor='secondary'>Heading 1</Heading>
+          <Heading size={2} textColor='secondary'>Heading 2</Heading>
+          <Heading size={3} textColor='secondary'>Heading 3</Heading>
+          <Heading size={4} textColor='secondary'>Heading 4</Heading>
+          <Heading size={5} textColor='secondary'>Heading 5</Heading>
+          <Text size={6} textColor='secondary'>Standard text</Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+        <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
+          <Heading size={6} textColor='secondary' caps>Standard List</Heading>
           <List>
             <ListItem>Item 1</ListItem>
             <ListItem>Item 2</ListItem>
@@ -74,7 +74,7 @@ export default class Presentation extends React.Component {
             <ListItem>Item 4</ListItem>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+        <Slide transition={['fade']} bgColor='secondary' textColor='primary'>
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite>Author</Cite>
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 /* eslint-disable */
 
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
   entry: [
-    "webpack-hot-middleware/client",
-    "babel-polyfill",
-    "./index"
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
+    './index'
   ],
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/dist/"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -22,22 +22,22 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.md$/,
-      loader: "html-loader!markdown-loader?gfm=false"
+      loader: 'html-loader!markdown-loader?gfm=false'
     }, {
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
-      loader: "babel-loader",
+      loader: 'babel-loader',
       query: {
         plugins: [
           [
-            "react-transform", {
+            'react-transform', {
               transforms: [{
-                transform: "react-transform-hmr",
-                imports: ["react"],
-                locals: ["module"]
+                transform: 'react-transform-hmr',
+                imports: ['react'],
+                locals: ['module']
               }, {
-                transform: "react-transform-catch-errors",
-                imports: ["react", "redbox-react"]
+                transform: 'react-transform-catch-errors',
+                imports: ['react', 'redbox-react']
               }]
             }
           ]
@@ -47,24 +47,24 @@ module.exports = {
       include: __dirname
     }, {
       test: /\.css$/,
-      loaders: ["style-loader", "raw-loader"],
+      loaders: ['style-loader', 'raw-loader'],
       include: __dirname
     }, {
       test: /\.svg$/,
-      loader: "url-loader?limit=10000&mimetype=image/svg+xml",
-      include: path.join(__dirname, "assets")
+      loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      include: path.join(__dirname, 'assets')
     }, {
       test: /\.png$/,
-      loader: "url-loader?mimetype=image/png",
-      include: path.join(__dirname, "assets")
+      loader: 'url-loader?mimetype=image/png',
+      include: path.join(__dirname, 'assets')
     }, {
       test: /\.gif$/,
-      loader: "url-loader?mimetype=image/gif",
-      include: path.join(__dirname, "assets")
+      loader: 'url-loader?mimetype=image/gif',
+      include: path.join(__dirname, 'assets')
     }, {
       test: /\.jpg$/,
-      loader: "url-loader?mimetype=image/jpg",
-      include: path.join(__dirname, "assets")
+      loader: 'url-loader?mimetype=image/jpg',
+      include: path.join(__dirname, 'assets')
     }]
   }
 };
